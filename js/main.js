d473efdcb646c8a2d2c99b6e01edbf6879d2da4c
Kobe Amoh <kobexamoh@gmail.com>
2020-05-04 02:13:19 -0600

    Add files via upload
    Update index page for 2020 along with new styles, images, JS, and documents.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
-var studentClick = document.querySelector('.showcase');
-studentClick.addEventListener('click', toggleCourses);
+// initialize and declare the variable(s)
+var hamburgerOpen = document.querySelector('#hamburger-open');
+var hamburgerClose = document.querySelector('#hamburger-close');
+var mobileNavigationMenu = document.querySelector('.navigation-block');
 
-var courseListing = document.querySelector('.course-listings');
+// add event listeners
+hamburgerOpen.addEventListener('click', menuOpen);
+hamburgerClose.addEventListener('click', menuClose);
 
-function toggleCourses(evt) {
+// create the functions
+function menuOpen() {
+    console.log('working!');
 
-    if (courseListing.classList.contains('hidden')) {
-        courseListing.classList.remove('hidden');
-        evt.preventDefault();
-    } else {
-        courseListing.classList.add('hidden');
-    }
-    evt.preventDefault();
+    // hide the 'hamburger-open' menu icon
+    hamburgerOpen.style.display = 'none';
+
+    // reveal the 'hamburger-close' menu icon
+    hamburgerClose.style.display = 'block';
+
+    // reveal the mobile navigation
+    mobileNavigationMenu.style.display = 'block';
+}
+
+function menuClose() {
+    console.log('working!');
+
+    // hide the 'hamburger-close' menu icon
+    hamburgerClose.style.display = 'none';
+
+    // reveal the 'hamburger-open' menu icon
+    hamburgerOpen.style.display = 'block';
+
+    // hide the mobile navigation
+    mobileNavigationMenu.style.display = 'none';
 }
